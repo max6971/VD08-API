ff64ce27499525efb752b1722c04fd988772046d
max6971 <cmytt@mail.ru>
2024-08-22 13:03:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, jsonify
 import requests
+from googletrans import Translator
 
 app = Flask(__name__)
+translator = Translator()
 
 @app.route('/')
 def home():
@@ -16,12 +18,16 @@ def get_quote():
         data = response.json()
         quote = data.get('content')
         author = data.get('author')
+        # Переводим цитату на русский
+        translated = translator.translate(quote, src='en', dest='ru')
+        translated_quote = translated.text
     else:
         quote = "Не удалось получить цитату."
         author = ""
+        translated_quote = quote
 
     # Возвращаем данные в формате JSON для использования в AJAX-запросе
-    return jsonify(quote=quote, author=author)
+    return jsonify(quote=quote, translated_quote=translated_quote, author=author)
 
 if __name__ == '__main__':
     app.run(debug=True)
